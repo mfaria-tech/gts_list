89e5f15ebd35c227988044d542ba462b2e40af79
Marcus Faria <mfaria@ness.com.br>
2025-05-30 16:06:10 -0300

    feat(components/loading.tsx): Created loading component

---
+++ src/app/_components/loading.tsx
@@ -0,0 +1,12 @@
+"use client";
+import React from "react";
+
+const Loading: React.FC = () => {
+  return (
+    <div className="group-loader">
+      <div className="loader"></div>
+    </div>
+  );
+};
+
+export default Loading;
