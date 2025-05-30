f360f572955614d4784d01494007735666706217
Marcus Faria <mfaria@ness.com.br>
2025-05-30 15:10:41 -0300

    feat(app/page.tsx): Created application home page

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+import { useEffect, useState } from "react";
+
+export default function Home() {
+
+  return (
+    <main className="main">
+      <h1 className="title">Lista de GTs</h1>
+    </main>
+  );
+}
